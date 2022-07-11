9ab01ab1e0169e682125775f621a6ae9715c9140
YaroslavSizov <106393442+YaroslavSizov@users.noreply.github.com>
2022-07-11 20:20:17 +0300

    add modal

---
+++ index.js
@@ -0,0 +1,29 @@
+// auto paddingTop
+// const { height: pageHeaderHeight } = document
+//     .querySelector(".header")
+//     .getBoundingClientRect();
+
+// document.body.style.paddingTop = `${pageHeaderHeight}px`;
+
+
+
+// openModalBtn
+const refs = {
+    openModalBtn: document.querySelector("[data-open-modal]"),
+    closeModalBtn: document.querySelector("[data-close-modal]"),
+    backdrop: document.querySelector("[data-backdrop]"),
+};
+
+refs.openModalBtn.addEventListener("click", toggleModal);
+refs.closeModalBtn.addEventListener("click", toggleModal);
+
+refs.backdrop.addEventListener("click", logBackdropClick);
+
+function toggleModal() {
+    refs.backdrop.classList.toggle("is-hidden");
+}
+
+function logBackdropClick() {
+    console.log("Это клик в бекдроп");
+}
+
