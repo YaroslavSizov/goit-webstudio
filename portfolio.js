1798e67cb21f2b6d02757fa109a7fa6a0853a980
YaroslavSizov <106393442+YaroslavSizov@users.noreply.github.com>
2022-08-08 22:16:22 +0300

    add adaptive

--- portfolio.js
+++ portfolio.js
@@ -5,29 +5,6 @@
 
 // document.body.style.paddingTop = `${pageHeaderHeight}px`;
 
-
-
-// openModalBtn
-const refs = {
-    openModalBtn: document.querySelector("[data-open-modal]"),
-    closeModalBtn: document.querySelector("[data-close-modal]"),
-    backdrop: document.querySelector("[data-backdrop]"),
-};
-
-refs.openModalBtn.addEventListener("click", toggleModal);
-refs.closeModalBtn.addEventListener("click", toggleModal);
-
-refs.backdrop.addEventListener("click", logBackdropClick);
-
-function toggleModal() {
-    refs.backdrop.classList.toggle("is-hidden");
-}
-
-function logBackdropClick() {
-    console.log("Это клик в бекдроп");
-}
-
-
 // openMenu
 (() => {
     const menuBtnRef = document.querySelector("[data-menu-button]");
